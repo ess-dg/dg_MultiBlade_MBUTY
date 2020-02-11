47515c9f9454389660f6e794338a4325f9118918
francescopiscitelli <francesco.piscitelli@esss.se>
2020-02-11 14:36:31 +0100

    added a test script to load VMM files

---
+++ LoadVMMdatatest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 13:59:56 2020
+
+@author: francescopiscitelli
+"""
+
+import numpy as np
+import pandas as pd
+
+
+datapathinput  = '/Users/francescopiscitelli/Documents/DOC/DATA/2020_02/VMM_testData/'
+
+filename = 'AmBeSource1526gdgem-readouts-20190819-152708_00000.h5'
+
+Clockd = 1e-9  # clock is 1ns
+
+DATA = np.array(pd.read_hdf((datapathinput+filename),'srs_hits'))
+
+TIME = (DATA[:,2]+DATA[:,8])*Clockd   # in s
+
+# check ch num has to start from 0 
+
+CH = DATA[:,3]
+ADC = DATA[:,6]
